5f3d9607355ad2c1b059c1f10cb9a20579d0e124
sndnvaps <sndnvaps@gmail.com>
2015-03-03 19:19:50 +0800

    Change to smaller ttf font   Fix some devices Battery & CPU temp display position
    Change-Id: I84c71211abe02af97b4943038dc9cb9f4966ec65
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- gui/devices/sed_ui.xml.sh
+++ gui/devices/sed_ui.xml.sh
@@ -3,12 +3,12 @@
 #修改支持中文字体
 
 #sed -i "s/<placement x=\"270\" y=\"76\" \/>/<placement x=\"400\" y=\"76\" \/>/" "$2"
-sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"30\" fallback=\"Roboto-Condensed-30\" \/>/<resource name=\"font\" type=\"font\" filename=\"RuHeiZhong.ttf\" size=\"28\" \/>/" "$1" 
+sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"30\" fallback=\"Roboto-Condensed-30\" \/>/<resource name=\"font\" type=\"font\" filename=\"SG_Bold.ttf\" size=\"28\" \/>/" "$1"
 
-sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"40\" fallback=\"Roboto-Condensed-40\" \/>/<resource name=\"font\" type=\"font\" filename=\"RuHeiZhong.ttf\" size=\"32\" \/>/" "$1" 
+sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"40\" fallback=\"Roboto-Condensed-40\" \/>/<resource name=\"font\" type=\"font\" filename=\"SG_Bold.ttf\" size=\"32\" \/>/" "$1"
 
-sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"12\" fallback=\"Roboto-Condensed-12\" \/>/<resource name=\"font\" type=\"font\" filename=\"RuHeiZhong.ttf\" size=\"10\" \/>/" "$1" 
+sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"12\" fallback=\"Roboto-Condensed-12\" \/>/<resource name=\"font\" type=\"font\" filename=\"SG_Bold.ttf\" size=\"10\" \/>/" "$1"
 
-sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"20\" fallback=\"Roboto-Condensed-20\" \/>/<resource name=\"font\" type=\"font\" filename=\"RuHeiZhong.ttf\" size=\"15\" \/>/" "$1" 
+sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"20\" fallback=\"Roboto-Condensed-20\" \/>/<resource name=\"font\" type=\"font\" filename=\"SG_Bold.ttf\" size=\"15\" \/>/" "$1"
 
 
