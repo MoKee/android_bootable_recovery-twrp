0024b70cfdfd6f8e0973c94276648f67a03b7a2f
sndnvaps <sndnvaps@gmail.com>
2015-05-20 00:22:01 +0800

    set def timezone to utf+8
    Change-Id: I2f4e36f1bd6de8a44987f56587c7c6d0586f6ce6
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- data.cpp
+++ data.cpp
@@ -755,7 +755,7 @@ void DataManager::SetDefaultValues()
 	mValues.insert(make_pair(TW_COLOR_THEME_VAR, make_pair("0", 1)));
 	mValues.insert(make_pair(TW_USE_COMPRESSION_VAR, make_pair("0", 1)));
 	mValues.insert(make_pair(TW_SHOW_SPAM_VAR, make_pair("0", 1)));
-	mValues.insert(make_pair(TW_TIME_ZONE_VAR, make_pair("CST6CDT,M3.2.0,M11.1.0", 1)));
+    mValues.insert(make_pair(TW_TIME_ZONE_VAR, make_pair("TAIST-8;TAIDT", 1)));
 	mValues.insert(make_pair(TW_SORT_FILES_BY_DATE_VAR, make_pair("0", 1)));
 	mValues.insert(make_pair(TW_GUI_SORT_ORDER, make_pair("1", 1)));
 	mValues.insert(make_pair(TW_RM_RF_VAR, make_pair("0", 1)));
