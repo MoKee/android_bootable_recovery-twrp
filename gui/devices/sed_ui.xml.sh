c255098b30db674e08198251d46799b389aa7632
sndnvaps <sndnvaps@gmail.com>
2016-01-09 23:50:05 +0800

    twrp::display ttf: replace BMFY_BOLD.ttf with YaHeiGB.ttf
    Change-Id: Ia646af1e7f1196d1c769e7950913f96a36e3b514
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- gui/devices/sed_ui.xml.sh
+++ gui/devices/sed_ui.xml.sh
@@ -4,27 +4,27 @@
 
 # New version for android 6.0 
 # 1920*1200 
-sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"30\" \/>/<resource name=\"font\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"30\" \/>/" "$1"
+sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"30\" \/>/<resource name=\"font\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"30\" \/>/" "$1"
 
-sed -i "s/<resource name=\"fixed\" type=\"font\" filename=\"DroidSansMono.ttf\" size=\"22\" \/>/<resource name=\"fixed\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"22\" \/>/" "$1"
+sed -i "s/<resource name=\"fixed\" type=\"font\" filename=\"DroidSansMono.ttf\" size=\"22\" \/>/<resource name=\"fixed\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"22\" \/>/" "$1"
 # 1080*1920 
-sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"40\" \/>/<resource name=\"font\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"40\" \/>/" "$1"
-sed -i "s/<resource name=\"mediumfont\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"40\" \/>/<resource name=\"mediumfont\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"40\" \/>/" "$1"
-sed -i "s/<resource name=\"filelist\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"40\" \/>/<resource name=\"filelist\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"40\" \/>/" "$1"
-sed -i "s/<resource name=\"fixed\" type=\"font\" filename=\"DroidSansMono.ttf\" size=\"30\" \/>/<resource name=\"fixed\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"30\" \/>/" "$1"
+sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"40\" \/>/<resource name=\"font\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"40\" \/>/" "$1"
+sed -i "s/<resource name=\"mediumfont\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"40\" \/>/<resource name=\"mediumfont\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"40\" \/>/" "$1"
+sed -i "s/<resource name=\"filelist\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"40\" \/>/<resource name=\"filelist\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"40\" \/>/" "$1"
+sed -i "s/<resource name=\"fixed\" type=\"font\" filename=\"DroidSansMono.ttf\" size=\"30\" \/>/<resource name=\"fixed\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"30\" \/>/" "$1"
 # 800*400 
-sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"16\" \/>/<resource name=\"font\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"16\" \/>/" "$1"
-sed -i "s/<resource name=\"fixed\" type=\"font\" filename=\"DroidSansMono.ttf\" size=\"12\" \/>/<resource name=\"fixed\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"12\" \/>/" "$1"
+sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"16\" \/>/<resource name=\"font\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"16\" \/>/" "$1"
+sed -i "s/<resource name=\"fixed\" type=\"font\" filename=\"DroidSansMono.ttf\" size=\"12\" \/>/<resource name=\"fixed\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"12\" \/>/" "$1"
 
 # 480*800 
-sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"20\" \/>/<resource name=\"font\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"20\" \/>/" "$1"
-sed -i "s/<resource name=\"mediumfont\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"20\" \/>/<resource name=\"font\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"20\" \/>/" "$1"
-sed -i "s/<resource name=\"filelist\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"20\" \/>/<resource name=\"font\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"20\" \/>/" "$1"
-sed -i "s/<resource name=\"fixed\" type=\"font\" filename=\"DroidSansMono.ttf\" size=\"16\" \/>/<resource name=\"fixed\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"16\" \/>/" "$1"
+sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"20\" \/>/<resource name=\"font\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"20\" \/>/" "$1"
+sed -i "s/<resource name=\"mediumfont\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"20\" \/>/<resource name=\"font\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"20\" \/>/" "$1"
+sed -i "s/<resource name=\"filelist\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"20\" \/>/<resource name=\"font\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"20\" \/>/" "$1"
+sed -i "s/<resource name=\"fixed\" type=\"font\" filename=\"DroidSansMono.ttf\" size=\"16\" \/>/<resource name=\"fixed\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"16\" \/>/" "$1"
 # 320*320
-sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"14\" \/>/<resource name=\"font\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"14\" \/>/" "$1"
-sed -i "s/<resource name=\"mediumfont\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"14\" \/>/<resource name=\"font\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"14\" \/>/" "$1"
-sed -i "s/<resource name=\"filelist\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"14\" \/>/<resource name=\"font\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"14\" \/>/" "$1"
-sed -i "s/<resource name=\"fixed\" type=\"font\" filename=\"DroidSansMono.ttf\" size=\"12\" \/>/<resource name=\"fixed\" type=\"font\" filename=\"BMFY_BOLD.ttf\" size=\"12\" \/>/" "$1"
+sed -i "s/<resource name=\"font\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"14\" \/>/<resource name=\"font\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"14\" \/>/" "$1"
+sed -i "s/<resource name=\"mediumfont\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"14\" \/>/<resource name=\"font\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"14\" \/>/" "$1"
+sed -i "s/<resource name=\"filelist\" type=\"font\" filename=\"RobotoCondensed-Regular.ttf\" size=\"14\" \/>/<resource name=\"font\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"14\" \/>/" "$1"
+sed -i "s/<resource name=\"fixed\" type=\"font\" filename=\"DroidSansMono.ttf\" size=\"12\" \/>/<resource name=\"fixed\" type=\"font\" filename=\"YaHeiGB.ttf\" size=\"12\" \/>/" "$1"
 
 
