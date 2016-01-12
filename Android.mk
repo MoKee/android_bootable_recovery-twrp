4dc59a2bc96ad46afb5bf26a3c686ec426ccc66d
sndnvaps <sndnvaps@gmail.com>
2016-01-12 23:25:46 +0800

    twrp:mkm-dev *fix build issuse of Chinese support    TW_BUILD_ZH_CN_SUPPORT := true    When TW_BUILD_ZH_CN_SUPPORT := true, is set ...    Set recovery-twrp/Android.mk , Add LOCAL_CFLAGS += -DTW_DEFAULT_LANGUAGE=cn
    Change-Id: I1ef69f4bdd6237eabe00bc1f9fe80a9988b716f8
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- Android.mk
+++ Android.mk
@@ -335,13 +335,11 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -gt 22; echo $$?),0)
 endif
 ifneq ($(TW_DEFAULT_LANGUAGE),)
     LOCAL_CFLAGS += -DTW_DEFAULT_LANGUAGE=$(TW_DEFAULT_LANGUAGE)
+else ifneq ($(TW_BUILD_ZH_CN_SUPPORT),)
+	 LOCAL_CFLAGS += -DTW_DEFAULT_LANGUAGE=cn
 else
     LOCAL_CFLAGS += -DTW_DEFAULT_LANGUAGE=en
 endif
-
-ifneq ($(TW_BUILD_ZH_CN_SUPPORT),)
-	LOCAL_CFLAGS += -DTW_BUILD_ZH_CN_SUPPORT
-endif 
 
 LOCAL_ADDITIONAL_DEPENDENCIES := \
     dump_image \
@@ -369,7 +367,7 @@ else
     LOCAL_LDFLAGS += -Wl,-dynamic-linker,/sbin/linker64
 endif
 ifneq ($(TW_USE_TOOLBOX), true)
-    LOCAL_ADDITIONAL_DEPENDENCIES += busybox_symlinks
+	LOCAL_ADDITIONAL_DEPENDENCIES += busybox_symlinks
 else
     ifneq ($(wildcard external/toybox/Android.mk),)
         LOCAL_ADDITIONAL_DEPENDENCIES += toybox_symlinks
@@ -380,7 +378,7 @@ else
     ifneq ($(wildcard external/unzip/Android.mk),)
         LOCAL_ADDITIONAL_DEPENDENCIES += unzip
     endif
-endif
+endif #end TW_USE_TOOLBOX 
 ifneq ($(TW_NO_EXFAT), true)
     LOCAL_ADDITIONAL_DEPENDENCIES += mkexfatfs fsckexfat
 endif
