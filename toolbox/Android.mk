de902a08766c9a334884a8f09fad976f2f874b49
dianlujitao <dianlujitao@gmail.com>
2016-01-26 20:01:27 +0800

    Don't include getprop and setprop when using toybox
     * They're included by default when using TW_USE_TOOLBOX=true
    
    Change-Id: Ie037d40c1c3a2e3e789a13b9c7ad7dcc95bd14ad

--- toolbox/Android.mk
+++ toolbox/Android.mk
@@ -205,17 +205,17 @@ ifneq (,$(filter $(PLATFORM_SDK_VERSION), 21 22 23))
     LOCAL_WHOLE_STATIC_LIBRARIES := $(patsubst %,libtoolbox_%,$(BSD_TOOLS))
 endif
 
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -gt 22; echo $$?),0)
-    # Rule to make getprop and setprop in M trees where toybox normally
-    # provides these tools. Toybox does not allow for easy dynamic
-    # configuration, so we would have to include the entire toybox binary
-    # which takes up more space than is necessary so long as we are still
-    # including busybox.
-    LOCAL_SRC_FILES += \
-        ../../../$(TWRP_TOOLBOX_PATH)/getprop.c \
-        ../../../$(TWRP_TOOLBOX_PATH)/setprop.c
-    OUR_TOOLS += getprop setprop
-    ifneq ($(TW_USE_TOOLBOX), true)
+ifneq ($(TW_USE_TOOLBOX), true)
+    ifeq ($(shell test $(PLATFORM_SDK_VERSION) -le 22; echo $$?),0)
+        # Rule to make getprop and setprop in M trees where toybox normally
+        # provides these tools. Toybox does not allow for easy dynamic
+        # configuration, so we would have to include the entire toybox binary
+        # which takes up more space than is necessary so long as we are still
+        # including busybox.
+        LOCAL_SRC_FILES += \
+            ../../../$(TWRP_TOOLBOX_PATH)/getprop.c \
+            ../../../$(TWRP_TOOLBOX_PATH)/setprop.c
+        OUR_TOOLS += getprop setprop
         LOCAL_SRC_FILES += ls.c
     endif
 endif
